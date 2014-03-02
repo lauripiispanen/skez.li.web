238962739a1f70515a07f23dad21ba0d358c6a53
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2014-03-02 16:50:40 +0200

    redirect non-www requests to www.skez.li

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 
 express()
+    .use(function(req, res, next) {
+        if (req.headers.host && req.headers.host.indexOf("skez.li") == 0) {
+            res.writeHead(302, {
+                'Location': 'http://www.skez.li'
+            });
+            res.end();
+        } else {
+            next();
+        }
+    })
     .use(express.static(__dirname + '/public'))
     .listen(process.env.PORT || 3000)
